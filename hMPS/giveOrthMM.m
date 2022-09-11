a821b127a01463e3468e3595f5bcf4a9cd360a5b
claudia-dela <65703581+claudia-dela@users.noreply.github.com>
2022-09-12 01:28:24 +0200

    update matrix of orth. space

--- hMPS/giveOrthMM.m
+++ hMPS/giveOrthMM.m
@@ -1,12 +1,13 @@
 %Input: 
 % A - hMPS tensors
+% N - number of sites
 % Output: 
 % MM - kernel of the matrix TA, contructed via Formula (6.16)
 % The columns of TA span the tangent space to the gauge orbit.
 % MM is the orthogonal complement in the domain of the
 % parametrization
-function [MM] = giveOrthMM(A)
-	[Q,R]=qr(STang(A),0);
+function [MM] = giveOrthMM(A,N)
+	[Q,R]=qr(STang(A,N),0);
 	kk=Q;
 	MM=null(kk');
 end
