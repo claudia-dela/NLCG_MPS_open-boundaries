921c3300e1895e355d80b53aaea064430f1741e8
claudia-dela <65703581+claudia-dela@users.noreply.github.com>
2022-09-12 01:36:10 +0200

    updates orth. space

--- hMPS/check_orth_complement.m
+++ hMPS/check_orth_complement.m
@@ -1,6 +1,7 @@
 %% Test tangent space to the gauge orbit and orthogonal complement
 d=3;                % physical dimension
 D=2;                % bond dimension
+N=5;                % number of sites
 
 n = 2*D + d*D^2 ;   % domain dim(D_{hMPS})
 s= 2*D+d*D^2 -D^2;  % dim(D_{hMPS})-dim(G), G = gauge group
@@ -12,7 +13,7 @@ Bfull = TensorB(d,D,n);
 v0=randn(1,n);%+1i*randn(1,n);  % random starting vector 
 A = BuildA(Bfull,v0); %point  (v_L,A,v_R) = Q in C^{dim(D_{hMPS})}
 
-[Q,R]=qr(STang(A),0);
-[MM] = giveOrthMM(A); %vector space orthogonal to the tangent direstions of the gauge orbit
+[Q,R]=qr(STang(A,N),0);
+[MM] = giveOrthMM(A,N); %vector space orthogonal to the tangent direstions of the gauge orbit
         
 disp(['Norm of Q^{\dag}MM: ',num2str(norm(Q'*MM))])
